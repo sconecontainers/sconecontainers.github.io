2e047888807bd243efeb701a22e7e4c6d0becf65
JanineKa <52662282+JanineKa@users.noreply.github.com>
2021-10-29 11:51:51 +0200

    Added: Demo Videos EN/DE

--- js/loadmore.js
+++ js/loadmore.js
@@ -3,7 +3,7 @@
     $(".content").slice(0, 2).show();
     $("#loadMore").on("click", function(e){
       e.preventDefault();
-      $(".content:hidden").slice(0, 1).slideDown();
+      $(".content:hidden").slice(0, 2).slideDown();
       if($(".content:hidden").length == 0) {
         $("#loadMore").text("No more older posts available").addClass("noContent");
       }
